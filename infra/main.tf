bf7f3a69275e2371903a046166bfdabc88c7e276
johanycastrop <johany.castro@uniminuto.edu.co>
2025-04-04 20:42:35 -0500

    Primer Commit

--- infra/main.tf
+++ infra/main.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "my_bucket" {
   // el nombre del bucket va a ser el nombre de la variable bucket_name
   // que se define en el archivo variables.tf
   // y se obtiene del archivo terraform.tfvars
-  bucket = var.bucket_name
+  bucket = var.s3infoiacgrupo1johany
 
   // se habilita el versionamiento del bucket
   versioning {
